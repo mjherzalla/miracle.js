ac75e3a7a85c49e05e19fe6061859fa8ae6ec1d0
mohammad herzalla <36680293+mjherzalla@users.noreply.github.com>
2018-03-27 02:14:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,25 @@ exports.getTableFromJson = function(Json)   {
             txt += "</table>"        
     return txt;
 }
+
+exports.sortList = function (ListID) {
+  var list, i, switching, b, shouldSwitch;
+  list = document.getElementById(ListID);
+  switching = true;
+  while (switching) {
+    switching = false;
+    b = list.getElementsByTagName("LI");
+    for (i = 0; i < (b.length - 1); i++) {
+    
+      shouldSwitch = false;
+      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        shouldSwitch= true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+}
